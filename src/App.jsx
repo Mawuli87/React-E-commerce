49f56eaaa78accc723848cee4815318be50db910
MobileContent.Com <mccmacbook@MobileContentComs-MacBook-Pro.local>
2025-05-20 17:23:21 +0000

    Responsive Navbard

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import NavItems from "./components/NavItems";
+
+function App() {
+  return (
+    <>
+      <NavItems />
+      <div className="min-vh-100">
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default App;
